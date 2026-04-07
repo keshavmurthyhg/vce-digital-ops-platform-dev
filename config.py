4ecdcc171576a92d092b99721b945ca06f8a25ce
keshavmurthyhg <keshavait@gmail.com>
2026-04-07 16:51:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,16 @@
 
 ENV = "DEV"   # change to TEST / PROD later
 
+ENV = "DEV"
+
 CONFIG = {
     "DEV": {
-        "SNOW_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/Snow-incidents.csv",
-        "PTC_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/PTC-Cases-Report.csv",
-        "AZURE_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/All-VCE-Bugs.csv",
+        "SNOW_URL": "https://raw.githubusercontent.com/keshavmurthyhg/vce-digital-ops-platform-dev/main/data/Snow-incidents.csv",
+        "PTC_URL": "https://raw.githubusercontent.com/keshavmurthyhg/vce-digital-ops-platform-dev/main/data/PTC-Cases-Report.csv",
+        "AZURE_URL": "https://raw.githubusercontent.com/keshavmurthyhg/vce-digital-ops-platform-dev/main/data/All-VCE-Bugs.csv"
     }
-    "TEST": {
+
+"TEST": {
         "SNOW_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/Snow-incidents.csv",
         "PTC_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/PTC-Cases-Report.csv",
         "AZURE_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/All-VCE-Bugs.csv",
@@ -18,4 +21,7 @@ CONFIG = {
         "PTC_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/PTC-Cases-Report.csv",
         "AZURE_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/All-VCE-Bugs.csv",      
     },
+
 }
+
+    
