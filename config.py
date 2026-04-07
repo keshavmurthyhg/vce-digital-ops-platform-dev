13fac8fa51e3c40122936a1691a086fd5185aac5
keshavmurthyhg <keshavait@gmail.com>
2026-04-07 16:17:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,15 +7,15 @@ CONFIG = {
         "SNOW_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/Snow-incidents.csv",
         "PTC_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/PTC-Cases-Report.csv",
         "AZURE_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/All-VCE-Bugs.csv",
-
+    }
     "TEST": {
         "SNOW_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/Snow-incidents.csv",
         "PTC_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/PTC-Cases-Report.csv",
         "AZURE_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/All-VCE-Bugs.csv",
-
+}
     "PROD": {
         "SNOW_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/Snow-incidents.csv",
         "PTC_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/PTC-Cases-Report.csv",
         "AZURE_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/All-VCE-Bugs.csv",      
     },
- }
+}
