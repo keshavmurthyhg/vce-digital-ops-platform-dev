e7c5776dfaf2def40ffd218c0f05db8de16d3cae
keshavmurthyhg <keshavait@gmail.com>
2026-04-07 16:56:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,17 +11,6 @@ CONFIG = {
         "AZURE_URL": "https://raw.githubusercontent.com/keshavmurthyhg/vce-digital-ops-platform-dev/main/data/All-VCE-Bugs.csv"
     }
 
-"TEST": {
-        "SNOW_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/Snow-incidents.csv",
-        "PTC_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/PTC-Cases-Report.csv",
-        "AZURE_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/All-VCE-Bugs.csv",
-}
-    "PROD": {
-        "SNOW_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/Snow-incidents.csv",
-        "PTC_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/PTC-Cases-Report.csv",
-        "AZURE_URL": "https://volvogroup.sharepoint.com/:x:/r/sites/TP-smartarchive/SmartArchive%20Documents/AMS-WINDCHILL/Keshava_L2/All-VCE-Bugs.csv",      
-    },
-
 }
 
     
