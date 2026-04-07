bdc4bf8be4732549afc28c7eb519f557201e499b
keshavmurthyhg <keshavait@gmail.com>
2026-04-07 18:55:04 +0530

    Update table.py

--- modules/table.py
+++ modules/table.py
@@ -6,8 +6,9 @@ def show_table(df):
         use_container_width=True,
         hide_index=True,
         df = df.reset_index(drop=True)
-        df.insert(0, "SL No", df.index + 1)
+        
         column_config={
+            df.insert(0, "SL No", df.index + 1)
             "Number": st.column_config.TextColumn(width="small"),
             "Description": st.column_config.TextColumn(width="large"),
             "Priority": st.column_config.TextColumn(width="small"),
