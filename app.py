b181789997c8c2e182be0338ecb33472af01cc2b
keshavmurthyhg <keshavait@gmail.com>
2026-04-12 23:34:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ total = len(filtered)
 page_size = 10
 total_pages = max((total + page_size - 1)//page_size,1)
 
-c1, c2, c3 = st.columns([10,11,12])
+c1, c2, c3 = st.columns([1,11,12])
 
 with c1:
     prev = st.button("◀")
