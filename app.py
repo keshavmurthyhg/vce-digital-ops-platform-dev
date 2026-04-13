e621be5b6ed89e695e7cf89a490051edaee593e1
keshavmurthyhg <keshavait@gmail.com>
2026-04-14 02:11:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -267,7 +267,7 @@ with colB:
 
     st.download_button("📥 Download Excel", to_excel(filtered), "ops_data.xlsx")
 
-#with colC:
+with colC:
  #   c1, c2, c3 = st.columns([1,2,1])
 
   #  if c1.button("◀") and st.session_state.page > 1:
@@ -279,6 +279,31 @@ with colB:
   #  if c3.button("▶") and st.session_state.page < total_pages:
     #    st.session_state.page += 1
      #   st.rerun()
+
+# ================= PAGINATION (NEW) =================
+
+col1, col2, col3 = st.columns([2,2,6])
+
+with col1:
+    page_size = st.selectbox(
+        "Rows",
+        [10, 20, 50, 100],
+        index=0
+    )
+
+total_rows = len(df_display)
+total_pages = max(1, (total_rows // page_size) + (1 if total_rows % page_size else 0))
+
+with col2:
+    page = st.selectbox(
+        "Page",
+        list(range(1, total_pages + 1))
+    )
+
+start = (page - 1) * page_size
+end = start + page_size
+
+page_df = df_display.iloc[start:end]
 
 # ================= TABLE =================
 st.write(page_df.to_html(escape=False, index=False), unsafe_allow_html=True)
