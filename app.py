9d126e483383cb1bef5b467de831ce0c6ccdbfa6
keshavmurthyhg <keshavait@gmail.com>
2026-04-22 06:04:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
+
 from modules.search.search_ui import render as search_ui
+from modules.analytics.charts_page import render as analytics_ui
 
 st.set_page_config(layout="wide")
 
 st.sidebar.title("Navigation")
 
-page = st.sidebar.radio("Go to", ["Home", "Search"])
+page = st.sidebar.radio("Go to", ["Home", "Search", "Analytics"])
 
 if page == "Home":
     st.title("🏠 Home")
@@ -13,3 +15,6 @@ if page == "Home":
 
 elif page == "Search":
     search_ui()
+
+elif page == "Analytics":
+    analytics_ui()
