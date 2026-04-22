33e27d29e2f86325b5c93d69bb916c7dc8ada94c
keshavmurthyhg <keshavait@gmail.com>
2026-04-22 05:40:21 +0530

    Update search_logic.py

--- modules/search/search_logic.py
+++ modules/search/search_logic.py
@@ -4,12 +4,15 @@ from modules.search.search import apply_search
 
 def apply_all_filters(df, status, priority, keyword):
 
+    # Status filter
     if status:
         df = df[df["Status"].isin(status)]
 
+    # Priority filter
     if priority:
         df = df[df["Priority"].isin(priority)]
 
+    # Keyword search
     df = apply_search(df, keyword)
 
     return df
