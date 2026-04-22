2d60888a36bfed0c2583f9953669c8991b632afd
keshavmurthyhg <keshavait@gmail.com>
2026-04-22 06:50:04 +0530

    Update doc_generator_ui.py

--- modules/report/doc_generator_ui.py
+++ modules/report/doc_generator_ui.py
@@ -4,7 +4,7 @@ from io import BytesIO
 import zipfile
 import re
 
-from modules.snow_loader import load_snow_data
+from modules.report.snow_loader import load_snow_data
 from modules.report.doc_generator import generate_word, generate_pdf_report
 
 
