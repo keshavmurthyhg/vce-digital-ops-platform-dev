b8cca546d0e49836a79f5d3b147f0cb68c0d9c87
keshavmurthyhg <keshavait@gmail.com>
2026-04-23 06:20:13 +0530

    Update doc_generator.py

--- modules/report/doc_generator.py
+++ modules/report/doc_generator.py
@@ -286,6 +286,8 @@ def generate_pdf(data, root, l2, res, images=None):
         ('VALIGN',(0,0),(-1,-1),'TOP'),   # 🔥 important
     ]))
 
+    elements.append(desc_table)
+    
    # BULLETS
     def add_bullets(text):
         for line in (text or "-").split("\n"):
