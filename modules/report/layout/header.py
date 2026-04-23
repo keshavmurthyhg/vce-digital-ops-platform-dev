e769da23d672de632f9dccdc7fb90b351fc4ffc3
keshavmurthyhg <keshavait@gmail.com>
2026-04-24 00:48:25 +0530

    Update header.py

--- modules/report/layout/header.py
+++ modules/report/layout/header.py
@@ -8,20 +8,39 @@ def wrap_link(field, value, styles):
 
 def build_pdf_header(data, wrap_link, format_date):
 
+    def wrap(x):
+        return Paragraph(str(x or ""), style=None)
+
     table = Table([
-        ["INCIDENT", wrap_link("incident", data.get("number")),
-         "CREATED BY", wrap_link(data.get("created_by"))],
-
-        ["AZURE BUG", wrap_link("azure", data.get("azure_bug")),
-         "CREATED DATE", wrap_link(format_date(data.get("created_date")))],
-
-        ["PTC CASE", wrap_link("ptc", data.get("ptc_case")),
-         "ASSIGNED TO", wrap_link(data.get("assigned_to"))],
-
-        ["PRIORITY", wrap_link(data.get("priority")),
-         "RESOLVED DATE", wrap_link(format_date(data.get("resolved_date")))],
+        [
+            "INCIDENT",
+            wrap_link("incident", data.get("number")),
+            "CREATED BY",
+            wrap(data.get("created_by"))
+        ],
+        [
+            "AZURE BUG",
+            wrap_link("azure", data.get("azure_bug")),
+            "CREATED DATE",
+            wrap(format_date(data.get("created_date")))
+        ],
+        [
+            "PTC CASE",
+            wrap_link("ptc", data.get("ptc_case")),
+            "ASSIGNED TO",
+            wrap(data.get("assigned_to"))
+        ],
+        [
+            "PRIORITY",
+            wrap(data.get("priority")),
+            "RESOLVED DATE",
+            wrap(format_date(data.get("resolved_date")))
+        ]
     ],
+                  
+    
     colWidths=[100, 160, 100, 160]   # ✅ correct place
+    
     )
 
     table.setStyle(TableStyle([
