bfeca51cec782934e547c1b2e6bbcf0b334b112e
keshavmurthyhg <keshavait@gmail.com>
2026-04-23 16:18:05 +0530

    Create description.py

---
+++ modules/report/layout/description.py
@@ -0,0 +1,25 @@
+from reportlab.platypus import Table, TableStyle, Paragraph
+from reportlab.lib import colors
+
+def build_pdf_description(data, center_style, clean_text, styles):
+
+    def wrap(x):
+        return Paragraph(str(x or ""), styles["Normal"])
+
+    table = Table([
+        [
+            Paragraph("<b>SHORT DESCRIPTION</b>", center_style),
+            Paragraph("<b>DESCRIPTION</b>", center_style)
+        ],
+        [
+            wrap(clean_text(data.get("short_description"))),
+            wrap(clean_text(data.get("description")))
+        ]
+    ], colWidths=[266,266])
+
+    table.setStyle(TableStyle([
+        ('GRID',(0,0),(-1,-1),1,colors.black),
+        ('BACKGROUND',(0,0),(-1,0),colors.lightgrey),
+    ]))
+
+    return table
