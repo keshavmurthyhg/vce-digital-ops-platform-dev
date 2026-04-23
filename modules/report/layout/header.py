1e06a25b9a4b0b3d882a80615026bbb1921e4c0c
keshavmurthyhg <keshavait@gmail.com>
2026-04-23 20:48:49 +0530

    Update header.py

--- modules/report/layout/header.py
+++ modules/report/layout/header.py
@@ -1,14 +1,9 @@
-from reportlab.platypus import Table, TableStyle, Paragraph
+from reportlab.platypus import Table, TableStyle
 from reportlab.lib import colors
-from reportlab.platypus import Table
-
-table = Table(
-    data,
-    colWidths=[120, 180, 120, 180]   # 👈 ADD THIS
-)
 
 
 def build_pdf_header(data, wrap_link, format_date):
+
     table = Table([
         ["INCIDENT", wrap_link(data.get("number")),
          "CREATED BY", wrap_link(data.get("created_by"))],
@@ -21,10 +16,12 @@ def build_pdf_header(data, wrap_link, format_date):
 
         ["PRIORITY", wrap_link(data.get("priority")),
          "RESOLVED DATE", wrap_link(format_date(data.get("resolved_date")))],
-    ], colWidths=[100,166,100,166])
+    ],
+    colWidths=[120, 180, 120, 180]   # ✅ correct place
+    )
 
     table.setStyle(TableStyle([
-        ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),   # header bold
+        ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
         ('FONTNAME', (0,0), (-1,-1), 'Helvetica'),
         ('GRID', (0,0), (-1,-1), 1, colors.black),
         ('VALIGN', (0,0), (-1,-1), 'MIDDLE'),
