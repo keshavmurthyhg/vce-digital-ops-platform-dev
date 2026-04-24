e2751160d8958bcec4df030b4ca657a3a613e8e5
keshavmurthyhg <keshavait@gmail.com>
2026-04-25 00:21:52 +0530

    Update combined_report.py

--- modules/converter/combined_report.py
+++ modules/converter/combined_report.py
@@ -8,24 +8,20 @@ from modules.report.doc_generator import generate_word_doc
 
 def generate_combined_report(ppt_path, snow_data, root, l2, res, images):
 
-    # 🔹 Generate SNOW doc
     snow_bytes = generate_word_doc(snow_data, root, l2, res, images)
 
-    # 🔹 Generate PPT doc
     ppt_temp = "temp_ppt.docx"
     ppt_to_word(ppt_path, ppt_temp)
 
     final_doc = Document()
 
-    # 🔹 Merge SNOW
     snow_doc = Document(BytesIO(snow_bytes))
-    for el in snow_doc.element.body:
-        final_doc.element.body.append(el)
+    for p in snow_doc.paragraphs:
+        final_doc.add_paragraph(p.text)
 
-    # 🔹 Merge PPT
     ppt_doc = Document(ppt_temp)
-    for el in ppt_doc.element.body:
-        final_doc.element.body.append(el)
+    for p in ppt_doc.paragraphs:
+        final_doc.add_paragraph(p.text)
 
     os.remove(ppt_temp)
 
