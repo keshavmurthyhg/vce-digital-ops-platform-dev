fd9d2720598f80c91b1bfb1e6e4591654d8ef54a
keshavmurthyhg <keshavait@gmail.com>
2026-04-24 18:55:10 +0530

    Update doc_to_pdf.py

--- modules/converter/doc_to_pdf.py
+++ modules/converter/doc_to_pdf.py
@@ -1,15 +1,16 @@
 from docx2pdf import convert
 
-def doc_to_pdf(docx_path, pdf_path):
-    convert(docx_path, pdf_path)
+#def doc_to_pdf(docx_path, pdf_path):
+#    convert(docx_path, pdf_path)
 
-#import subprocess
+import subprocess
+import os
 
-#def doc_to_pdf_libreoffice(docx_path, output_dir):
- #   subprocess.run([
-  #      "soffice",
- #       "--headless",
- #       "--convert-to", "pdf",
- #       "--outdir", output_dir,
- #       docx_path
- #   ])
+def doc_to_pdf(docx_path, output_dir):
+    subprocess.run([
+        "soffice",
+        "--headless",
+        "--convert-to", "pdf",
+        "--outdir", output_dir,
+        docx_path
+    ])
