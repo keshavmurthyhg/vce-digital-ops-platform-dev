af3f340e9492b00241b14ec738c68289e97b32d3
keshavmurthyhg <keshavait@gmail.com>
2026-04-28 17:01:19 +0530

    Update styles.py

--- modules/report/ui/styles.py
+++ modules/report/ui/styles.py
@@ -1,7 +1,3 @@
-TABLE_STYLE = """
-border:2px solid black;
-border-collapse: collapse;
-font-size:14px;
-"""
-
+TABLE_STYLE = "width:100%; border-collapse: collapse; font-size:14px; border:2px solid black;"
 CELL_STYLE = "border:1px solid black; padding:6px;"
+HEADER_CELL_STYLE = "border:1px solid black; padding:6px; font-weight:bold;"
