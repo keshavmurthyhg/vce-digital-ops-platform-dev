65b386e328317679a4c673ff3a8a2f50ce558d87
keshavmurthyhg <keshavait@gmail.com>
2026-04-29 02:26:21 +0530

    Update components.py

--- modules/common/ui/components.py
+++ modules/common/ui/components.py
@@ -1,60 +1,57 @@
 import streamlit as st
-
-from modules.common.ui.styles import TABLE_STYLE, CELL_STYLE, HEADER_CELL_STYLE
 from modules.common.utils.links import make_ui_link
+from modules.common.utils.formatters import safe_text, format_date
 from modules.common.utils.text_cleaner import format_description
-from modules.common.utils.formatters import safe_text
 
 
 def render_preview_table(data):
 
-    html = f"""
-    <table style="{TABLE_STYLE}">
-        <tr>
-            <td style="{HEADER_CELL_STYLE}">INCIDENT</td>
-            <td style="{CELL_STYLE}">{make_ui_link("incident", data.get("number"))}</td>
-            <td style="{HEADER_CELL_STYLE}">CREATED BY</td>
-            <td style="{CELL_STYLE}">{safe_text(data.get("created_by"))}</td>
-        </tr>
-        <tr>
-            <td style="{HEADER_CELL_STYLE}">AZURE BUG</td>
-            <td style="{CELL_STYLE}">{make_ui_link("azure", data.get("azure_bug"))}</td>
-            <td style="{HEADER_CELL_STYLE}">CREATED DATE</td>
-            <td style="{CELL_STYLE}">{safe_text(data.get("created_date"))}</td>
-        </tr>
-        <tr>
-            <td style="{HEADER_CELL_STYLE}">PTC CASE</td>
-            <td style="{CELL_STYLE}">{safe_text(data.get("ptc_case"))}</td>
-            <td style="{HEADER_CELL_STYLE}">ASSIGNED TO</td>
-            <td style="{CELL_STYLE}">{safe_text(data.get("assigned_to"))}</td>
-        </tr>
-        <tr>
-            <td style="{HEADER_CELL_STYLE}">PRIORITY</td>
-            <td style="{CELL_STYLE}">{safe_text(data.get("priority"))}</td>
-            <td style="{HEADER_CELL_STYLE}">RESOLVED DATE</td>
-            <td style="{CELL_STYLE}">{safe_text(data.get("resolved_date"))}</td>
-        </tr>
-    </table>
-    """
+    st.markdown("""
+    <table style="width:100%; border-collapse: collapse; border:2px solid black;">
+    """, unsafe_allow_html=True)
 
-    st.markdown(html, unsafe_allow_html=True)
+    st.markdown(f"""
+    <tr>
+        <td><b>INCIDENT</b></td>
+        <td>{make_ui_link("incident", data.get("number"))}</td>
+        <td><b>CREATED BY</b></td>
+        <td>{safe_text(data.get("created_by"))}</td>
+    </tr>
+    <tr>
+        <td><b>AZURE BUG</b></td>
+        <td>{make_ui_link("azure", data.get("azure_bug"))}</td>
+        <td><b>CREATED DATE</b></td>
+        <td>{format_date(data.get("created_date"))}</td>
+    </tr>
+    <tr>
+        <td><b>PTC CASE</b></td>
+        <td>{safe_text(data.get("ptc_case"))}</td>
+        <td><b>ASSIGNED TO</b></td>
+        <td>{safe_text(data.get("assigned_to"))}</td>
+    </tr>
+    <tr>
+        <td><b>PRIORITY</b></td>
+        <td>{safe_text(data.get("priority"))}</td>
+        <td><b>RESOLVED DATE</b></td>
+        <td>{format_date(data.get("resolved_date"))}</td>
+    </tr>
+    </table>
+    """, unsafe_allow_html=True)
 
 
 def render_description_table(data):
 
     desc = format_description(data.get("description"))
 
-    html = f"""
-    <table style="{TABLE_STYLE}">
-        <tr>
-            <td style="{HEADER_CELL_STYLE}">SHORT DESCRIPTION</td>
-            <td style="{HEADER_CELL_STYLE}">DESCRIPTION</td>
-        </tr>
-        <tr>
-            <td style="{CELL_STYLE}">{safe_text(data.get("short_description"))}</td>
-            <td style="{CELL_STYLE}">{safe_text(desc)}</td>
-        </tr>
+    st.markdown(f"""
+    <table style="width:100%; border-collapse: collapse; border:2px solid black;">
+    <tr>
+        <td><b>SHORT DESCRIPTION</b></td>
+        <td><b>DESCRIPTION</b></td>
+    </tr>
+    <tr>
+        <td>{safe_text(data.get("short_description"))}</td>
+        <td>{safe_text(desc)}</td>
+    </tr>
     </table>
-    """
-
-    st.markdown(html, unsafe_allow_html=True)
+    """, unsafe_allow_html=True)
