3e787b85c3544c9eff62a80938bb518ee3b1d069
keshavmurthyhg <keshavait@gmail.com>
2026-04-29 00:02:14 +0530

    Update rca_service.py

--- modules/report/services/rca_service.py
+++ modules/report/services/rca_service.py
@@ -1,4 +1,4 @@
-from modules.common.utils.text_cleaner import clean_description
+from modules.report.utils.utils import format_description
 from modules.common.utils.parsers import extract_azure_id
 from modules.report.domain.rca_generator import generate_rca
 
@@ -7,7 +7,7 @@ def build_rca(data):
 
     cleaned = {
         "short_description": data.get("short_description"),
-        "description": clean_description(data.get("description")),
+        "description": format_description(data.get("description")),
         "work_notes": data.get("work_notes"),
         "resolution": data.get("resolution"),
         "azure_bug": extract_azure_id(data.get("resolution"))
