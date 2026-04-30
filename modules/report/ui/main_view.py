66d2966238accfaeb986fb415f507ef3c2341899
keshavmurthyhg <keshavait@gmail.com>
2026-05-01 03:31:19 +0530

    Update main_view.py

--- modules/report/ui/main_view.py
+++ modules/report/ui/main_view.py
@@ -25,7 +25,7 @@ def render_main(df):
             """
             <style>
             div[data-testid="stButton"] {
-                margin-top: 15px;
+                margin-top: 12px;
             }
             </style>
             """,
