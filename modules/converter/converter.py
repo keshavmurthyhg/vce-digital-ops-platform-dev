ab2737bbf549e1ea9485e201e455d88275bea241
keshavmurthyhg <keshavait@gmail.com>
2026-05-01 21:49:47 +0530

    Update converter.py

--- modules/converter/converter.py
+++ modules/converter/converter.py
@@ -2,17 +2,20 @@ import os
 from modules.converter.ppt_to_doc import ppt_to_word
 from modules.converter.doc_to_pdf import doc_to_pdf
 
+
 def convert_ppt(ppt_path, output_folder):
     base = os.path.splitext(os.path.basename(ppt_path))[0]
 
     docx_path = os.path.join(output_folder, f"{base}.docx")
-    pdf_path = os.path.join(output_folder, f"{base}.pdf")
 
     ppt_to_word(ppt_path, docx_path)
+
+    pdf_path = None
+
     try:
         doc_to_pdf(docx_path, output_folder)
         pdf_path = os.path.join(output_folder, f"{base}.pdf")
-    except Exception as e:
-        pdf_path = None
+    except:
+        pass
 
     return docx_path, pdf_path
