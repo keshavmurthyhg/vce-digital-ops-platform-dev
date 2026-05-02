71243b50175947d155ba5a822eb08645ebc2fe30
keshavmurthyhg <keshavait@gmail.com>
2026-05-02 21:00:01 +0530

    Update word_renderer.py

--- modules/report/renderers/word_renderer.py
+++ modules/report/renderers/word_renderer.py
@@ -247,11 +247,11 @@ def generate_word_doc(
                 )
     
                 # Align with table start/end points
-                p.paragraph_format.left_indent = Inches(0.3)
-                p.paragraph_format.right_indent = Inches(0.3)
+                p.paragraph_format.left_indent = Inches(0.2)
+                p.paragraph_format.right_indent = Inches(0.2)
                 
                 # Better bullet formatting
-                p.paragraph_format.first_line_indent = Inches(0)
+                #p.paragraph_format.first_line_indent = Inches(0)
                 
                 # spacing
                 p.paragraph_format.space_after = Pt(4)
