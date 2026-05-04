f9a9a1be1bab65b0b8765ff2757d2c95738b0c02
keshavmurthyhg <keshavait@gmail.com>
2026-05-04 20:55:50 +0530

    Update ui.py

--- modules/word_compare/ui.py
+++ modules/word_compare/ui.py
@@ -28,7 +28,7 @@ def render():
                 font-size:42px;
                 font-weight:700;
                 color:#2f3342;
-                margin-top:3px;
+                margin-top:4px;
                 white-space:nowrap;
                 line-height:1;
             ">
