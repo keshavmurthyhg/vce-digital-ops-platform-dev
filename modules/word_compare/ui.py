81dae4bd0438c7569aeea4ed8db4e645960692d3
keshavmurthyhg <keshavait@gmail.com>
2026-05-04 22:39:57 +0530

    Update ui.py

--- modules/word_compare/ui.py
+++ modules/word_compare/ui.py
@@ -155,6 +155,7 @@ def render():
 
             st.sidebar.download_button(
                 label="Download Compared File",
+                use_container_width=True
                 data=output_data["file_bytes"],
                 file_name=output_data["file_name"],
                 mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document"
